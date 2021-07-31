c2160533ac3c4780f4a243f4fef8b0d99fc3d3b5
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-07-31 19:47:10 -0300

    v5

--- django_project/MeuBlog/models.py
+++ django_project/MeuBlog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_Length=255)
+    #title = models.CharField(max_Length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
