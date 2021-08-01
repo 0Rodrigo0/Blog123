edc7a1cc6d96b96aefc2cf75a31d05c8b0a39994
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-01 15:31:24 -0300

    v

--- django_project/MeuBlog/urls.py
+++ django_project/MeuBlog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPosView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('',  HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('faca_seu_post/', AddPosView.as_view(), name='Faca_Seu_Post'),
 
 ]
