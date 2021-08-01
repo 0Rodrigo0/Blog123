a67547d849f7a51fc8a6aaddc264d03dda89787a
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-01 18:30:42 -0300

    v

--- django_project/MeuBlog/forms.py
+++ django_project/MeuBlog/forms.py
@@ -12,3 +12,16 @@ class PostForm(forms.ModelForm):
             'author' : forms.Select(attrs={'class': 'form-control'}),
             'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escreva o texto aqui.'}),
         }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escreva seu titulo aqui.'}),
+            'title_tag' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escreva sua Tag aqui.'}),
+            #'author' : forms.Select(attrs={'class': 'form-control'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escreva o texto aqui.'}),
+        }
--- django_project/MeuBlog/urls.py
+++ django_project/MeuBlog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPosView
+from .views import HomeView, ArticleDetailView, AddPosView, UpdatePostView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('',  HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('faca_seu_post/', AddPosView.as_view(), name='Faca_Seu_Post'),
-
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
