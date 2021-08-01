e07cdebdb184935bcf16c27777c0dad27bfd390d
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-01 18:53:52 -0300

    v

--- django_project/MeuBlog/urls.py
+++ django_project/MeuBlog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPosView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPosView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     #path('', views.home, name="home"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('faca_seu_post/', AddPosView.as_view(), name='Faca_Seu_Post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 ]
