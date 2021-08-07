da3d903dd2bac9390d1e3395334ca7f9e0a59e97
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-07 16:06:27 -0300

    correção cabeçalho post

--- django_project/MeuBlog/urls.py
+++ django_project/MeuBlog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPosView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('',  HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-    path('faca_seu_post/', AddPosView.as_view(), name='Faca_Seu_Post'),
+    path('faca_seu_post/', AddPostView.as_view(), name='Faca_Seu_Post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 ]
