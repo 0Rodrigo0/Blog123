6771cce0f4c1781346e636a6a0c46f1337268027
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-07 09:55:25 -0300

    v

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf#hos7l7twpo%3u(h+&8%e_8b$0@a&-abxq5-nlbghl5^#zu2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG') 
 
 ALLOWED_HOSTS = []
 
