403edb7188c4addb5e772d2d5fa9d08de84557c4
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-07 14:51:02 -0300

    Com login e verificações de logout

--- django_project/MeuBlog/views.py
+++ django_project/MeuBlog/views.py
@@ -10,8 +10,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    #ordering = ['-id']
-    ordering = ['post_date']
+    ordering = ['-id']
+    #ordering = ['post_date ']
 
 class ArticleDetailView(DetailView):
     model = Post
