1f06639952c666b1889ab43b6bbff2e396d8fb74
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-15 11:31:45 -0300

    com snippet

--- django_project/MeuBlog/forms.py
+++ django_project/MeuBlog/forms.py
@@ -12,7 +12,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escreva seu titulo aqui.'}),
@@ -21,6 +21,7 @@ class PostForm(forms.ModelForm):
             #'author' : forms.Select(attrs={'class': 'form-control'}),
             'category' : forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escreva o texto aqui.'}),
+            'snippet' : forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 
