0c7a53b1b210776418685e1895a03b6ce6fe05f7
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-08-15 15:46:26 -0300

    com alterar senha

--- django_project/members/views.py
+++ django_project/members/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('login')
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -10,7 +15,8 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    #form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
     def get_object(self):
