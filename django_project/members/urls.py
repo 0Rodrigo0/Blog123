7f330b5426eb452914587f16db559f38be0bf8ae
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-09-07 11:19:05 -0300

    comentarios

--- django_project/members/urls.py
+++ django_project/members/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('password_success', views.password_success, name="password_success"),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_proflie_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
-    path('/create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
+    path('create_profile_page', CreateProfilePageView.as_view(), name='create_profile_page'),
 
 ]
