e9660806a8407dae1819b3c7b2c5149c1b09cf6f
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-09-12 00:19:47 -0300

    com erro de profile corrigido

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG') 
 
-ALLOWED_HOSTS = ['meuublog.herokuapp.com']
+ALLOWED_HOSTS = ['meuublog.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
